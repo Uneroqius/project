0c28e0c527206650ac3f7d49bedf17900c5f4eef
Uneroqius <61160248+Uneroqius@users.noreply.github.com>
2022-05-19 05:09:39 +0300

    Create Form.1.Designer.cs
    //Данный код приведён ниже, потому что происходили изменения в доступе к 
    //БД, также представлена часть кода, которая была изменена.

---
+++ c#/Form.1.Designer.cs
@@ -0,0 +1,11 @@
+public CourseDataSet courseDataSet;
+public System.Windows.Forms.BindingSource _1BindingSource;
+public CourseDataSetTableAdapters._1TableAdapter _1TableAdapter;
+public CourseDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+public System.Windows.Forms.BindingNavigator _1BindingNavigator;
+public System.Windows.Forms.DataGridView _1DataGridView;
+public System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+public System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+public System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+public System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+public System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
